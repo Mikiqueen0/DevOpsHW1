e4af177ba39fc7ad128a8000a29ab8df16077485
Mikiqueen0 <107250330+Mikiqueen0@users.noreply.github.com>
2026-02-03 04:48:40 +0700

    add docker file

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from main.models import PreviousJob
 
-# Create your views here.
 def landing_page(request):
-  return render(request, 'main/landing.html')
-
+    users = User.objects.all()
+    context = {
+      "users": users,
+      "jobs": PreviousJob.objects.all(),
+    }
+    return render(request, 'main/landing.html', context)
